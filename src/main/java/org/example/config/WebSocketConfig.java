1ef96dec7c9ef258f80e1bee1e30e66159e0e5e8
rafael <rafaelmagalhaes1@live.com>
2024-06-15 13:20:18 -0300

    feat: WebSocketConfig created

---
+++ src/main/java/org/example/config/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package org.example.config;
+
+import org.example.handler.ChatWebSocketHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+/**
+ * The type Web socket config.
+ */
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+  @Override
+  public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+    registry.addHandler(
+        new ChatWebSocketHandler(), "/chat"
+    ).setAllowedOrigins("*");
+  }
+}
+
