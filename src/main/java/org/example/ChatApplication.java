5a55e0e9cd19d33f9cb2358130da6cd714461a59
rafael <rafaelmagalhaes1@live.com>
2024-06-15 13:20:03 -0300

    initial commit

---
+++ src/main/java/org/example/ChatApplication.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The type Chat application.
+ */
+@SpringBootApplication
+public class ChatApplication {
+
+  /**
+   * The entry point of application.
+   *
+   * @param args the input arguments
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(ChatApplication.class, args);
+  }
+}
